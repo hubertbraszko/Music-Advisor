1778699c1c0ee6572c1832a07c037412b49d176e
hubertbraszko <49716983+hubertbraszko@users.noreply.github.com>
2021-10-03 23:10:14 +0200

    Update Config.java

--- Music Advisor/task/src/advisor/Config.java
+++ Music Advisor/task/src/advisor/Config.java
@@ -5,7 +5,7 @@ public enum Config {
     SERVER_PATH("https://accounts.spotify.com"),
     AUTHORIZE_PATH("/authorize"),
     REDIRECT_URI("http://localhost:8080/"),
-    CLIENT_ID("890f9c4b0e8f4843b4a49b4b7180247f"),
+    CLIENT_ID(""),
     CLIENT_SECRET(""),
     ACCESS_TOKEN(""),
     AUTH_CODE(""),
